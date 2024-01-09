6992cf25d07d533034842f1b890c0c791742dd7d
JavierMagana9 <javier.magana9@gmail.com>
2024-01-09 05:09:11 +0100

    readme, css, y json documentacion actualizado

--- README.md
+++ README.md
@@ -1,1 +1,157 @@
 # PracticaFiltrarImagenes
+
+Lo primero que he tenido en cuenta es que elementos del documento HTML iban a ser estaticos y cuales dinamicos. 
+```html
+<body>
+    <header>
+        <h1>Práctica Filtro</h1>
+    </header>   
+    <main>
+        <h2>FOTOS</h2>
+        <div id="botones">
+            
+        </div>
+       
+        <div id="fotoGrande">
+
+        </div>
+        <h3>IMAGENES RELACIONADAS</h3>
+        <div id="galeria">
+
+        </div>
+    </main>
+    <footer>
+        <p>footer</p>
+    </footer>
+    <script src="js/main.js"></script>
+</body>
+```
+
+Una vez deidido, elegí que elementos del DOM tenía que capturar para usar posteriormente:
+
+```js
+const botones = document.querySelector('#botones')
+const fotoGrande = document.querySelector('#fotoGrande')
+const galeria = document.querySelector('#galeria')
+```
+
+Despues decidi que habia que crear para posteriormente pintar, en este caso un array con las fotos 
+
+```js
+const arrayImg = [
+    {
+        foto: "assets/viajes-1.jpg",
+        alter: "mujer tumbada en amaca en la playa",
+        titulo: "título del viaje 1",
+        etiqueta: ["mar", "arena"]
+    }
+    ...
+]
+```
+### Funciones
+
+una vez decidido esto, pense que acciones necesitaba:
+
+1. pintar las fotos pequeñas:
+
+con un forEach recorro cada objeto del array y a su vez cada item, es decir cada objeto.
+
+Mediante el metodo .find(devuelve el valor del primer elemento del array) busco en todas las etiquetas de cada objeto para ver si coinciden (con el metodo .includes, que devuelve true si encuentra lo que busca) con boton. 
+
+Si es asi, se pintan div y dentro de el img y p con el titulo, src y alt correspondientes.
+
+Todo se crea en el elemnto estatico div galeria.
+El innerHTML vacio al principio de la funcion impide que se acumulen una y otra vez cada vez que se hace click en los botones.
+
+```js
+
+const muestraImg = (boton) => {
+
+    galeria.innerHTML = ''
+
+    arrayImg.forEach((item) => {
+        
+        const comparacion = item.etiqueta.find((tag) =>
+            tag.includes(boton)
+        )
+        if (comparacion == boton) {
+
+            const caja = document.createElement('DIV')
+            const foto = document.createElement('IMG')
+            const titulo = document.createElement('P')
+            titulo.textContent = item.titulo
+            foto.src = item.foto
+            foto.alt = item.alter
+            caja.append(titulo)
+            caja.append(foto)
+            fragment.append(caja)
+
+        }
+
+        
+    })
+    
+    galeria.append(fragment)
+}
+
+```
+
+2. pintar la foto grande:
+
+aqui creo una foto y la coloco dentro de mi div fotoGrande. El innerHTML vacio al principio de la funcion impide que se acumulen una y otra vez cada vez que se hace click en los botones. 
+
+```js
+const muestraFotoGrande = (fotoArriba) => {
+    fotoGrande.innerHTML = ''
+
+    const foto = document.createElement('IMG')
+    foto.src = fotoArriba
+    fragment.append(foto)
+    fotoGrande.append(fragment)
+
+}
+```
+3. crear botones 
+
+aunque arrayBotones debia estar creado de forma dinamica, aqui lo utilizo desde una variable universal. Un forEach itera el array creando un boton por cada valor, recibiendo su contenido e id. 
+
+```js
+const creaBotones = () => {
+
+
+    arrayBotones.forEach((item) => {
+        const button = document.createElement('BUTTON');
+        button.textContent = item;
+        button.id = item;
+
+        fragment.append(button)
+    })
+
+    botones.append(fragment)
+}
+```
+
+### Eventos
+Una vez decididas las funciones tenia que ver cuando se ejecutarian. Cree una delegacion de eventos. Al hacer click en cada boton y al hacer click en cada imagen.
+Gracias a la expresion de target podemos capturar
+```js
+document.addEventListener('click', (ev) => {
+
+    const target = ev.target;
+    const fotoArriba = ev.target.src
+    if (target.matches("button")) {
+
+        const boton = ev.target.id
+        //console.log(etiqueta)
+        muestraImg(boton)
+        muestraFotoGrande(fotoArriba)
+        fraseExp()
+    } else if (target.matches("img")) {
+
+        muestraFotoGrande(fotoArriba)
+    }
+
+});
+```
+
+
--- js/main.js
+++ js/main.js
@@ -1,70 +1,108 @@
+
+
 //1.Cuales son los elementos con los que voy a interactuar, botones y fotos
+/**
+ * Captura de elementos del DOM para interactuar con ellos
+ */
 const botones = document.querySelector('#botones')
 
-const frase = document.querySelector('#frase')
 const fotoGrande = document.querySelector('#fotoGrande')
 const galeria = document.querySelector('#galeria')
-const titulo = document.querySelector('h3')
+
+
 //FRAGMENT
+/**
+ * Creación de un fragmento de documento para agilizar y no hacer tantas llamadas al DOM
+ */
 const fragment = document.createDocumentFragment()
 //2.Que es lo que pintaremos luego: necesito pintar las imagenes asociadas al tema y pintar una mas grande que otra.
-const arrayBotones = ["mar", "arena","señales", "edificio", "montaña"]
+const arrayBotones = ["mar", "arena", "señales", "edificio", "montaña", "cosa"]
 
+/**
+ * Array que contiene objetos que son imágenes.
+ */
 const arrayImg = [
     {
         foto: "assets/viajes-1.jpg",
         alter: "mujer tumbada en amaca en la playa",
         titulo: "título del viaje 1",
-        id: ["mar", "arena"]
+        etiqueta: ["mar", "arena"]
     },
     {
         foto: "assets/viajes-2.jpg",
         alter: "Bungaloo sobre las olas",
         titulo: "título del viaje 2",
-        id: "mar"
+        etiqueta: ["mar"]
     },
     {
         foto: "assets/viajes-3.jpg",
         alter: "Cruce de caminos",
         titulo: "título del viaje 3",
-        id: "señales"
+        etiqueta: ["señales", 'arena']
     },
     {
         foto: "assets/viajes-4.jpg",
         alter: "Plaza España en Sevilla",
         titulo: "título del viaje 4",
-        id: "edificio"
+        etiqueta: ["edificio"]
     },
     {
         foto: "assets/viajes-5.jpg",
         alter: "Plaza España en otro angulo",
         titulo: "título del viaje 5",
-        id: "edificio"
+        etiqueta: ["edificio"]
     },
     {
         foto: "assets/viajes-6.jpg",
         alter: "Paseo maritimo junto a las montañas",
         titulo: "título del viaje 6",
-        id: ["mar", "arena", "montaña"]
+        etiqueta: ["mar", "arena", "montaña"]
     },
     {
         foto: "assets/viajes-7.jpg",
         alter: "Castillo coronando el altode una colina, abajo un pueblo",
         titulo: "título del viaje 7",
-        id: ["edificio", "montaña"]
+        etiqueta: ["edificio", "montaña"]
     }
 ]
 
-//4.cuando se realizaran las acciones? al hacer 'click' tanto en los botones como en las nuevas fotos para que se hagan grandes 
-document.addEventListener("click", (ev)=>{
-    
-})
-//3.Que acciones necesito?? Pintar botones, pintar las fotos, pintar fots en grande,  pintar la frase, filtrar imagenes y eliminar el titulo.
 
+//4.cuando se realizaran las acciones? al hacer 'click' tanto en los botones como en las nuevas fotos para que se hagan grandes
+
+/**
+ * Escucha de eventos 'click' en el documento para manipular imágenes y botones.
+ */
+document.addEventListener('click', (ev) => {
+
+    const target = ev.target;
+    // @ts-ignore
+    const fotoArriba = ev.target.src
+    // @ts-ignore
+    if (target.matches("button")) {
+        // @ts-ignore
+        const boton = ev.target.id
+        //console.log(etiqueta)
+        muestraImg(boton)
+        muestraFotoGrande(fotoArriba)
+        fraseExp()
+    // @ts-ignore
+    } else if (target.matches("img")) {
+
+        muestraFotoGrande(fotoArriba)
+    }
+
+});
+
+
+//3.Que acciones necesito?? Pintar botones,crear de forma dinamica array, pintar las fotos, pintar fots en grande,  pintar la frase, filtrar imagenes y eliminar el titulo.
+/**
+ * Crea botones dinámicamente y los añade al DOM.
+ */
 const creaBotones = () => {
+
+
     arrayBotones.forEach((item) => {
         const button = document.createElement('BUTTON');
-        button.value = item;
         button.textContent = item;
         button.id = item;
 
@@ -73,26 +111,86 @@ const creaBotones = () => {
 
     botones.append(fragment)
 }
-creaBotones()
 
+/**
+ * Muestra imágenes filtradas basadas en la etiqueta del botón seleccionado.
+ * @param {string} boton
+ */
 
-const muestraImg = () => {
-   
+const muestraImg = (boton) => {
 
-    arrayImg.forEach((item)=>{
-        const caja = document.createElement("DIV")
-        const foto = document.createElement("IMG")
-        foto.src = item.foto
-        foto.alt = item.alter
-        foto.id = item.id
-        caja.append(foto)
-        fragment.append(caja)
+    galeria.innerHTML = ''
+
+    arrayImg.forEach((item) => {
+        //console.log(item)      
+        // @ts-ignore
+        const comparacion = item.etiqueta.find((tag) =>
+            tag.includes(boton)
+
+            //console.log(item.etiqueta)
+        )
+        if (comparacion == boton) {
+            //console.log("hola")
+
+            const caja = document.createElement('DIV')
+            const foto = document.createElement('IMG')
+            const titulo = document.createElement('P')
+            titulo.textContent = item.titulo
+            // @ts-ignore
+            foto.src = item.foto
+            // @ts-ignore
+            foto.alt = item.alter
+            caja.append(titulo)
+            caja.append(foto)
+            fragment.append(caja)
+
+        }
+
+        //console.log(boton)
+        //console.log(comparacion)
+        //console.log(arrayEncontrados)
     })
+    //console.log({arrayEncontrados})
     galeria.append(fragment)
+    fraseExp()
 }
-muestraImg()
 
+const fraseExp = (boton) => {
 
-const desaparece = () => {
+arrayImg 
+
 
 }
+/*const primeraFoto = (fotoArriba) => {
+
+}*/
+
+/**
+ * Muestra una imagen en un tamaño más grande.
+ * @param {string} fotoArriba
+ */
+const muestraFotoGrande = (fotoArriba) => {
+    fotoGrande.innerHTML = ''
+
+    const foto = document.createElement('IMG')
+    // @ts-ignore
+    foto.src = fotoArriba
+    // @ts-ignore
+    //caja.append(foto)
+    // @ts-ignore
+    fragment.append(foto)
+    fotoGrande.append(fragment)
+
+}
+
+/*
+const arrayEtiquetas = () => {
+    
+    const arrayEt = []
+    .forEach()
+}
+*/
+
+
+
+creaBotones()
